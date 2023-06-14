b3fc33cc94e6fa210ba5392a90bbc90bb9cce1ae
diyorbek700 <abduganiyevdiyorbek360@gmail.com>
2023-06-14 11:58:55 +0500

    Tavel in React

---
+++ react-website-yt-1/src/App.js
@@ -0,0 +1,29 @@
+import React from "react";
+import Navbar from "./Navbar";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Home from "./components/pages/Home";
+import Services from "./components/pages/Services";
+import Products from "./components/pages/Products";
+import SignUp from "./components/pages/SignUp";
+
+
+
+
+function App() {
+  return (
+    <>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" exact Component={Home} />
+           <Route path='/services' Component={Services}></Route>
+           <Route path='/products' Component={Products}></Route>
+           <Route path='/sign-up' Component={SignUp}></Route>
+        </Routes>
+      </Router>
+    </>
+  );
+}
+
+export default App;
